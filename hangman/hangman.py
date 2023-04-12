0ba660c09be606c16764ca3e415794ae3b65a5f5
Yuliia Lysak <julia.m.lysak@gmail.com>
2023-04-12 15:19:31 +0300

    Hangman - Stage 2/8: Let’s play a game

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,2 +1,15 @@
-print("H A N G M A N")
-print("The game will be available soon.")
+def intro():
+    print("H A N G M A N")
+
+
+def game():
+    print("Guess the word:")
+    word = input()
+    if word == "python":
+        print("You survived!")
+    else:
+        print("You lost!")
+
+
+intro()
+game()
