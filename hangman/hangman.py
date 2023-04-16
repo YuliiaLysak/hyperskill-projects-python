ee6e15318d6087a414278af6ebd5c1e9fd204f1b
Yuliia Lysak <julia.m.lysak@gmail.com>
2023-04-16 20:13:09 +0300

    Hangman - Stage 4/8: Help is on the way

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -7,9 +7,19 @@ def intro():
     print("H A N G M A N")
 
 
+def get_word_to_guess():
+    return random.choice(words)
+
+
+def replace_with_hyphens(word):
+    hyphens_count = len(word) - 3
+    return word[0:3] + "-" * hyphens_count
+
+
 def game():
-    word_to_guess = random.choice(words)
-    print("Guess the word:")
+    word_to_guess = get_word_to_guess()
+    word_to_show = replace_with_hyphens(word_to_guess)
+    print("Guess the word: " + word_to_show)
     word = input()
     if word == word_to_guess:
         print("You survived!")
