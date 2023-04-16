bae18e0a70459ad1a4e80917ca4d000ab02ebf7e
Yuliia Lysak <julia.m.lysak@gmail.com>
2023-04-16 13:16:37 +0300

    Hangman - Stage 3/8: Make your choice

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,11 +1,17 @@
+import random
+
+words = "python", "java", "swift", "javascript"
+
+
 def intro():
     print("H A N G M A N")
 
 
 def game():
+    word_to_guess = random.choice(words)
     print("Guess the word:")
     word = input()
-    if word == "python":
+    if word == word_to_guess:
         print("You survived!")
     else:
         print("You lost!")
