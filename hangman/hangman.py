d39d2712bbc5078c8ab662baf9ee8b19834a4794
Yuliia Lysak <julia.m.lysak@gmail.com>
2023-04-17 17:31:02 +0300

    Hangman - Stage 8/8: Menu, please

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,18 +1,35 @@
 import random
 
 words = "python", "java", "swift", "javascript"
-input_letters = []
+score = {
+    "win": 0,
+    "lost": 0
+}
 
 
 def intro():
-    print("H A N G M A N")
+    print("H A N G M A N  # 8 attempts")
+
+
+def show_menu():
+    while True:
+        print('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:')
+        action = input()
+        if action == "play":
+            play_game()
+        elif action == "results":
+            show_results()
+        elif action == "exit":
+            break
+        else:
+            continue
 
 
 def get_word_to_guess():
     return list(random.choice(words))
 
 
-def validate_input(user_input):
+def validate_input(user_input, input_letters):
     if len(user_input) != 1:
         print("Please, input a single letter.")
         return False
@@ -37,9 +54,11 @@ def show_guessed_letters(letter, word_to_guess, guessed):
 
 def evaluate_result(guessed):
     if "-" in guessed:
+        score["lost"] += 1
         print()
         print("You lost!")
     else:
+        score["win"] += 1
         result = "".join(guessed)
         print()
         print(f"You guessed the word {result}!")
@@ -47,6 +66,7 @@ def evaluate_result(guessed):
 
 
 def play_game():
+    input_letters = []
     word_to_guess = get_word_to_guess()
     guessed = ["-"] * len(word_to_guess)
     allowed_mistakes = 8
@@ -55,7 +75,7 @@ def play_game():
         print("".join(guessed))
         print("Input a letter:")
         letter = input()
-        if not validate_input(letter):
+        if not validate_input(letter, input_letters):
             continue
         elif letter in word_to_guess:
             guessed = show_guessed_letters(letter, word_to_guess, guessed)
@@ -68,5 +88,10 @@ def play_game():
     evaluate_result(guessed)
 
 
+def show_results():
+    print(f'You won: {score["win"]} times')
+    print(f'You lost: {score["lost"]} times')
+
+
 intro()
-play_game()
+show_menu()
