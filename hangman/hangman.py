9e008fb702a8fc6e20eed33608f50620d4f17949
Yuliia Lysak <julia.m.lysak@gmail.com>
2023-04-17 17:03:52 +0300

    Hangman - Stage 7/8: Error!

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,6 +1,7 @@
 import random
 
 words = "python", "java", "swift", "javascript"
+input_letters = []
 
 
 def intro():
@@ -9,6 +10,21 @@ def intro():
 
 def get_word_to_guess():
     return list(random.choice(words))
+
+
+def validate_input(user_input):
+    if len(user_input) != 1:
+        print("Please, input a single letter.")
+        return False
+    elif not user_input.isalpha() or user_input.isupper():
+        print("Please, enter a lowercase letter from the English alphabet.")
+        return False
+    elif user_input in input_letters:
+        print("You've already guessed this letter.")
+        return False
+    else:
+        input_letters.append(user_input)
+        return True
 
 
 def show_guessed_letters(letter, word_to_guess, guessed):
@@ -24,9 +40,9 @@ def evaluate_result(guessed):
         print()
         print("You lost!")
     else:
+        result = "".join(guessed)
         print()
-        print("".join(guessed))
-        print("You guessed the word!")
+        print(f"You guessed the word {result}!")
         print("You survived!")
 
 
@@ -39,16 +55,15 @@ def play_game():
         print("".join(guessed))
         print("Input a letter:")
         letter = input()
-        if letter in guessed:
-            print("No improvements.")
-            allowed_mistakes -= 1
+        if not validate_input(letter):
+            continue
         elif letter in word_to_guess:
             guessed = show_guessed_letters(letter, word_to_guess, guessed)
             if "-" not in guessed:
                 break
         else:
-            print("That letter doesn't appear in the word.")
             allowed_mistakes -= 1
+            print(f"That letter doesn't appear in the word.  # {allowed_mistakes} attempts")
 
     evaluate_result(guessed)
 
